926ee64c7a31d7bea58efa04a8f1d227e58d11b5
sadoshi-279 <131440485+sadoshi-279@users.noreply.github.com>
2023-05-29 01:16:45 +0800

    Add files via upload

---
+++ pygame/import-2 sys.py
@@ -0,0 +1,31 @@
+import sys
+import pygame
+
+pygame.init()
+fps = 60
+fpsClock = pygame.time.Clock()
+screen = pygame.display.set_mode((1000,1000),pygame.FULLSCREEN,32)
+
+while  True:
+    screen.fill((110,123,139))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    #pygame.display.flip()
+    fpsClock.tick(fps)
+
+    rightimg = pygame.image.load("C:\\Users\\Hannah\\Untitled-2\\right.png")
+    screen.blit(pygame.transform.scale(rightimg, (100, 100)),(460,460))
+
+    leftimg = pygame.image.load("C:\\Users\\Hannah\\Untitled-2\\left.png")
+    screen.blit(pygame.transform.scale(leftimg, (100, 100)),(460,460))
+
+    upimg = pygame.image.load("C:\\Users\\Hannah\\Untitled-2\\up.png")
+    screen.blit(pygame.transform.scale(upimg, (100, 100)),(460,460))
+
+    downimg = pygame.image.load("C:\\Users\\Hannah\\Untitled-2\\down.png")
+    screen.blit(pygame.transform.scale(downimg, (100, 100)),(460,460))
+
+    pygame.display.flip()
