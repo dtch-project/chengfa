acacf13754277f9ad8501563d26ed41d98d34f0b
sadoshi-279 <131440485+sadoshi-279@users.noreply.github.com>
2023-05-29 23:53:43 +0800

    Update import-2 sys.py

--- pygame/import-2 sys.py
+++ pygame/import-2 sys.py
@@ -17,15 +17,15 @@ while  True:
     fpsClock.tick(fps)
 
     rightimg = pygame.image.load("C:\\Users\\Hannah\\Untitled-2\\right.png")
-    screen.blit(pygame.transform.scale(rightimg, (100, 100)),(460,460))
+    screen.blit(pygame.transform.scale(rightimg, (100, 100)),(510,460))
 
     leftimg = pygame.image.load("C:\\Users\\Hannah\\Untitled-2\\left.png")
-    screen.blit(pygame.transform.scale(leftimg, (100, 100)),(460,460))
+    screen.blit(pygame.transform.scale(leftimg, (100, 100)),(410,460))
 
     upimg = pygame.image.load("C:\\Users\\Hannah\\Untitled-2\\up.png")
-    screen.blit(pygame.transform.scale(upimg, (100, 100)),(460,460))
+    screen.blit(pygame.transform.scale(upimg, (100, 100)),(460,410))
 
     downimg = pygame.image.load("C:\\Users\\Hannah\\Untitled-2\\down.png")
-    screen.blit(pygame.transform.scale(downimg, (100, 100)),(460,460))
+    screen.blit(pygame.transform.scale(downimg, (100, 100)),(460,510))
 
     pygame.display.flip()
